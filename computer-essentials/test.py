de3068d91972519f9a7bbd5759c3f2d9952fc879
piratecat-lover <johngalt827@gmail.com>
2024-09-04 20:24:07 +0900

    Added computer-essentials file.

---
+++ computer-essentials/test.py
@@ -0,0 +1,7 @@
+# To print single quotes in a triple-quoted string, use backslash + single quote.
+# print('''\'fat
+#     cat\'''')
+
+# To print brackets in a f-string, use double brackets
+# a = 30
+# print(f"{{a}}") # {a}
